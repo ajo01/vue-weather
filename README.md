cfd4c9fb8f32675436424c9fe2ab834c35f6f877
ajo01 <amyjojjm2001@gmail.com>
2021-07-12 21:37:17 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+cd vue-weather
+npm run serve
+
+# vue-weather
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+  <main>
+    <div class="search-box">
+      <input type="text" name="" id="" class="search-bar" placeholder="Search">
+    </div>
+  </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "92bb94446ae83a4d0612357549eb65c9",
+    };
+  },
+};
+</script>
+
+<style>
+  *{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family:'montserrat',sans-serif;
+  }
+
+  #app{
+    background-image: url('./assets/cold-bg.jpeg');
+  }
+</style>
