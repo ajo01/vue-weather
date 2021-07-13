a0f8636dbb1175bfc1d121313cdc07d5f172a053
ajo01 <amyjojjm2001@gmail.com>
2021-07-12 22:17:35 -0700

    styling done

--- src/App.vue
+++ src/App.vue
@@ -112,6 +112,17 @@ export default {
     font-weight: 900;
     text-shadow:3px 6px rgba(0,0,0,0.25);
     background-color: rgba(255,255,255,0.25);
+    border-radius: 16px;
+    margin:30px 0;
+    box-shadow: 3px 6px rgba(0,0,0,0.25);
     }
+
+  .weather-box .weather{
+    color:white;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0,0,0,0.25);
+    } 
 
 </style>
