23893b096ce0ce1b2a9be7ec4535ce1a64bf226c
ajo01 <amyjojjm2001@gmail.com>
2021-07-12 22:12:14 -0700

    text styling

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,19 @@
     <div class="search-box">
       <input type="text" name="" id="" class="search-bar" placeholder="Search">
     </div>
+    <div class="weather-wrap">
+      <div class="location-box">
+        <div class="location">
+          Northampton, UK
+        </div>
+        <div class="date"> Monday 20 January 2021</div>
+      </div>
+
+      <div class="weather-box"> 
+        <div class="temp">9°C</div>
+        <div class="weather">Rain</div>
+      </div>
+    </div>
   </main>
   </div>
 </template>
@@ -60,6 +73,45 @@ export default {
     background: none;
     background-color: rgba(255,255,255,0.5);
     border-radius: 0px 16px 0px 16px;
+    transition: 0.4s;
+    box-shadow: 0 0 8px rgba(0,0,0,0.25);
   }
+
+  .search-box .search-bar:focus{
+    background-color: rgba(255,255,255,0.75);
+    box-shadow: 0 0 16px rgba(0,0,0,0.25);
+    border-radius: 16px 0 16px 0;
+  }
+
+  .location-box .location {
+    color:white;
+    font-size:33px;
+    font-weight:500;
+    text-align: center;
+    text-shadow:1px 3px rgba(0,0,0,0.25);
+  }
+
+  .location-box .date{
+    color:white;
+    font-size:20px;
+    font-weight:300;
+    text-align: center;
+    text-shadow:1px 3px rgba(0,0,0,0.25);
+    font-style: italic;
+  }
+
+  .weather-box {
+     text-align: center;
+  }
+
+  .weather-box .temp{
+    display: inline-block;
+    padding:10px 25px;
+    color:white;
+    font-size: 102px;
+    font-weight: 900;
+    text-shadow:3px 6px rgba(0,0,0,0.25);
+    background-color: rgba(255,255,255,0.25);
+    }
 
 </style>
